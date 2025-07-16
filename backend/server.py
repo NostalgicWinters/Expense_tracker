d33ecb05a83103a47c04a4eddc1181a8b0906519
NostalgicWinters <Dev.Sterling@outlook.com>
2025-07-16 11:58:20 +0530

    add projectdb.py

--- backend/server.py
+++ backend/server.py
@@ -1,12 +1,17 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/")
-def home():
-    return jsonify({"pages":["home","about us"]})
+@app.route("/signup", methods=["POST"])
+def signup():
+    data = request.get_json()
+    username = data.get("username")
+    email = data.get("email")
+    password = data.get("password")
+    if not username or not email or not password:
+        return {"error": "Missing fields"}, 400
 
 if __name__ == "__main__":
     app.run(debug=True)
