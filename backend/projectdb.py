cc15d1f184e77257cdeea3544b958c87d8b3145e
NostalgicWinters <Dev.Sterling@outlook.com>
2025-07-17 19:12:12 +0530

    connect balance and transactions

--- backend/projectdb.py
+++ backend/projectdb.py
@@ -9,9 +9,13 @@ mydb = pymysql.connect(
 
 mycursor = mydb.cursor()
 
-def signUp(username, email, password):
+def signUp(username, email, password, balance):
     mycursor.execute("INSERT INTO users (username, email, password) VALUES (%s, %s, %s)", (username, email, password))
     mydb.commit()
+    user_id = mycursor.lastrowid
+    mycursor.execute("INSERT INTO balance (user_id, balance) VALUES (%s,%s)",(user_id,balance))
+    mydb.commit()
+    return user_id
 
 def logIn(email, password):
     mycursor.execute("SELECT * FROM users WHERE email=%s AND password=%s", (email, password))
@@ -20,10 +24,14 @@ def logIn(email, password):
 def add_transaction(user_id, amount, description, category, date):
     mycursor.execute("INSERT INTO transactions (user_id, amount, description, category, date) VALUES (%s, %s, %s, %s, %s)", (user_id, amount, description, category, date))
     mydb.commit()
+    mycursor.execute("UPDATE balance SET balance = balance + %s WHERE user_id = %s",(amount,user_id))
+    mydb.commit()
 
-def delete_transaction(transaction_id):
+def delete_transaction(transaction_id, amount, user_id):
     mycursor.execute("DELETE FROM transactions WHERE id=%s", (transaction_id,))
-    mydb.commit()   
+    mydb.commit()
+    mycursor.execute("UPDATE balance SET balance = balance - %s WHERE user_id = %s",(amount,user_id))   
+    mydb.commit()
 
 def view_transactions(user_id):
     mycursor.execute("SELECT * FROM transactions WHERE user_id=%s", (user_id,))
